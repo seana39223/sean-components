97c68d61a4b9f0a2ddd02bf5ecb1eaf0a3763044
sean <seana39223@gmail.com>
2020-03-01 22:03:17 +0000

    feat: Has text property which has artist name over the top.

--- src/components/Artist/Artist.js
+++ src/components/Artist/Artist.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Artist.css';
 
 function Artist(props) {
-    console.log(props.image);
+    const [text, setText] = useState('');
     return (
         <div class='Artist'>
-            <img src={props.image}></img>
+            <img src={props.image}
+                 onMouseOver={() => setText(props.text)}
+                 onMouseLeave={() => setText('')}
+            ></img>
+            <p>{text}</p>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Artist from "./components/Artist/Artist";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Artist image={'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/02/11/screen-shot-2018-03-02-at-12.23.53.png?w968h681'} />, document.getElementById('root'));
+ReactDOM.render(<Artist image={'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/02/11/screen-shot-2018-03-02-at-12.23.53.png?w968h681'} text={'Example'} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
